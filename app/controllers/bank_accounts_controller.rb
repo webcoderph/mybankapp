9b80fb99f1fee47d3bf16fba7001bf13608b06ae
webcoderph <webcoderph@gmail.com>
2018-07-10 21:10:41 +0800

    added bank account create

--- app/controllers/bank_accounts_controller.rb
+++ app/controllers/bank_accounts_controller.rb
@@ -7,7 +7,35 @@ class BankAccountsController < ApplicationController
 
   def show; end
 
+  def new
+    @bank_account = BankAccount.new
+  end
+
+  def create
+    @bank_account = BankAccount.new(bank_account_params)
+   
+    if @bank_account.save
+      flash[:success] = "You have successfully added a new bank account"
+
+      redirect_to bank_account_path(@bank_account.id) 
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+  end
+
+  def destroy
+  end
+
   private
+
+  def bank_account_params
+    params.require(:bank_account).permit(:account_no, :client_id)  
+  end
 
   def load_bank_account
     @bank_account = BankAccount.find(params[:id])
